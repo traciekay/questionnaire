6bfc2d24f1519d7c7b431574891a394c2ec427a8
Tracy Kaonje Omamo <tomamo@SamaDc.corp>
2017-08-01 18:01:28 +0300

    updated new styles

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@
 $(document).ready(function(){
   $('a[title]').tooltip();
 });
+
+submitForms = function(){
+    document.getElementById("section0").submit();
+    document.getElementById("sectionA").submit();
+    document.getElementById("sectionB").submit();
+    document.getElementById("sectionC").submit();
+    document.getElementById("sectionD").submit();
+    document.getElementById("sectionE").submit();
+    document.getElementById("sectionF").submit();
+    document.getElementById("sectionG").submit();
+}
